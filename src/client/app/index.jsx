baa811599d06bff00074999cd73069519790d97f
Gennady Yonov <g.jonovs@mail.ru>
2018-01-17 21:19:17 +0200

    Setup React.js with Npm, Babel 6 and Webpack

---
+++ src/client/app/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class MemoryGame extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Memory Game coming soon...
+            </div>
+        );
+    }
+}
+
+render(<MemoryGame/>, document.getElementById('container'));
