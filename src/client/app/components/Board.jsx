f38554cc01c3dee7fd880f944321530df1056779
Gennady Yonov <g.jonovs@mail.ru>
2018-01-19 19:33:33 +0200

    Identify The Minimal (but complete) Representation Of UI State and Where It Should Live

--- src/client/app/components/Board.jsx
+++ src/client/app/components/Board.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ArrayUtils from './ArrayUtils.jsx';
 import StatusBar from './StatusBar.jsx';
 import Card from './Card.jsx';
 import Button from './Button.jsx';
@@ -6,10 +7,38 @@ import Rules from './Rules.jsx';
 
 export default class Board extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        const cards = this.initCards();
+        this.state = {cards: cards, attempt: 0};
+    }
+
+    initCards() {
+        const values = this.randomValues();
+        let cards = [];
+        for (let i = 0; i < values.length; i++) {
+            let card = {value: values[i], opened: false, locked: false};
+            cards.push(card);
+        }
+        return cards;
+    }
+
+    randomValues() {
+        const pairs = this.props.pairs;
+        let values = [];
+        for (let i = 0; i < pairs; i++) {
+            values.push(i);
+            values.push(i);
+        }
+        ArrayUtils.shuffle(values);
+        return values;
+    }
+
     render() {
         const message = this.statusMessage();
         const cards = [];
-        this.props.cards.forEach((card, index) => {
+        this.state.cards.forEach((card, index) => {
             cards.push(<Card key={index} card={card}/>);
         });
         return (
@@ -24,8 +53,8 @@ export default class Board extends React.Component {
 
     statusMessage() {
         const maxNumberOfAttempts = this.props.maxNumberOfAttempts;
-        const attempt = this.props.attempt;
-        const cards = this.props.cards;
+        const attempt = this.state.attempt;
+        const cards = this.state.cards;
         const left = maxNumberOfAttempts - attempt;
         if (left <= 0) {
             return 'You Loose...';
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -6,20 +6,9 @@ class MemoryGame extends React.Component {
 
     render() {
         return (
-            <Board cards={this.props.cards} maxNumberOfAttempts={10} attempt={0}/>
+            <Board pairs={4} maxNumberOfAttempts={10} attempt={0}/>
         );
     }
 }
 
-const CARDS = [
-    {value: 0, opened: false, locked: false},
-    {value: 0, opened: false, locked: false},
-    {value: 1, opened: true, locked: false},
-    {value: 1, opened: true, locked: false},
-    {value: 2, opened: false, locked: false},
-    {value: 2, opened: false, locked: false},
-    {value: 3, opened: false, locked: false},
-    {value: 3, opened: false, locked: false}
-];
-
-render(<MemoryGame cards={CARDS}/>, document.getElementById('container'));
+render(<MemoryGame/>, document.getElementById('container'));
